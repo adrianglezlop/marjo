466b334d124f0ca0536825b76240e97333bf0948
adrianglezlop <entornoc9@gmail.com>
2020-02-16 15:52:17 -0700

    doctos

--- app/pdf/recibo_pdf.rb
+++ app/pdf/recibo_pdf.rb
@@ -21,9 +21,9 @@ class ReciboPdf < Prawn::Document
       
       image "#{Rails.root}/app/assets/images/logo marjo.png", width: 155, height: 45, :at => [70,740+des]
       draw_text  "FOLIO:  2503616176B#{"0"*(5-ticket.id.to_s.length)+ticket.id.to_s}",:size=>9, :at => [420,720+des]
-      draw_text  "FECHA CARGO: #{ticket.payment.fecha_de_pago}" ,:size=>9, :at => [420,705+des]
-      draw_text  "SUCURSAL: #{ticket.payment.credit.padre.branch_office.nombre}",:size=>9, :at => [420,690+des]
-      draw_text  "AGENTE: #{ticket.payment.credit.padre.nombre_completo}",:size=>9, :at => [420,675+des]
+      draw_text  "FECHA : #{ticket.payment.fecha_de_pago}" ,:size=>9, :at => [420,705+des]
+      draw_text  "FUNDACION: #{ticket.payment.credit.padre.branch_office.nombre}",:size=>9, :at => [420,690+des]
+      draw_text  "PROMOTOR DE APOYO: #{ticket.payment.credit.padre.nombre_completo}",:size=>9, :at => [420,675+des]
   
       
       draw_text  "ORIGINAL ",:size=>8, :at => [500,735+des]
@@ -31,7 +31,8 @@ class ReciboPdf < Prawn::Document
       draw_text  "RECIBÍ DE : #{ticket.payment.credit.nombre_completo_deudor} ",:size=>9, :at => [70,665+des]
       draw_text  "LA CANTIDAD DE:  #{Dinero.to_money(ticket.cantidad)}",:size=>9, :at => [70,652+des]
       draw_text  "                     (#{(I18n.with_locale(:es) { (ticket.cantidad).to_i.to_words }).upcase } PESOS 00/100 M.N.)",:size=>9, :at => [70,639+des]
-      draw_text  "POR CONCEPTO DE: PAGO O ABONO AL CARGO #{ticket.payment.credit.product.payout.periocidad} #{ticket.payment.recibo} CORRESPONDIENTE AL CREDITO",:size=>9, :at => [70,626+des]
+      draw_text  "POR CONCEPTO DE: PAGO O ABONO AL CARGO #{ticket.payment.credit.product.payout.periocidad} #{ticket.payment.recibo} AL APOYO
+ECONÓMICO ",:size=>9, :at => [70,626+des]
       draw_text  "                  OTORGADO EL #{Auxiliar.fecha_formato(ticket.payment.credit.fecha_de_contrato)}",:size=>9, :at => [70,613+des]
       draw_text  "ATRASADO ACTUAL DE: #{Dinero.to_money at} ",:size=>9, :at => [70,600+des]
       
@@ -44,17 +45,18 @@ class ReciboPdf < Prawn::Document
       
       image "#{Rails.root}/app/assets/images/logo marjo.png", width: 155, height: 45, :at => [70,740+des]
       draw_text  "FOLIO:  2503616176B#{"0"*(5-ticket.id.to_s.length)+ticket.id.to_s}",:size=>9, :at => [420,720+des]
-      draw_text  "FECHA CARGO: #{ticket.payment.fecha_de_pago}" ,:size=>9, :at => [420,705+des]
-      draw_text  "SUCURSAL: #{ticket.payment.credit.padre.branch_office.nombre}",:size=>9, :at => [420,690+des]
-      draw_text  "AGENTE: #{ticket.payment.credit.padre.nombre_completo}",:size=>9, :at => [420,675+des]
+      draw_text  "FECHA : #{ticket.payment.fecha_de_pago}" ,:size=>9, :at => [420,705+des]
+      draw_text  "FUNDACION: #{ticket.payment.credit.padre.branch_office.nombre}",:size=>9, :at => [420,690+des]
+      draw_text  "PROMOTOR DE APOYO: #{ticket.payment.credit.padre.nombre_completo}",:size=>9, :at => [420,675+des]
   
       
-      draw_text  "COPIA 1 ",:size=>8, :at => [500,735+des]
+      draw_text  "ORIGINAL ",:size=>8, :at => [500,735+des]
       
       draw_text  "RECIBÍ DE : #{ticket.payment.credit.nombre_completo_deudor} ",:size=>9, :at => [70,665+des]
       draw_text  "LA CANTIDAD DE:  #{Dinero.to_money(ticket.cantidad)}",:size=>9, :at => [70,652+des]
       draw_text  "                     (#{(I18n.with_locale(:es) { (ticket.cantidad).to_i.to_words }).upcase } PESOS 00/100 M.N.)",:size=>9, :at => [70,639+des]
-      draw_text  "POR CONCEPTO DE: PAGO O ABONO AL CARGO #{ticket.payment.credit.product.payout.periocidad} #{ticket.payment.recibo} CORRESPONDIENTE AL CREDITO",:size=>9, :at => [70,626+des]
+      draw_text  "POR CONCEPTO DE: PAGO O ABONO AL CARGO #{ticket.payment.credit.product.payout.periocidad} #{ticket.payment.recibo} AL APOYO
+ECONÓMICO ",:size=>9, :at => [70,626+des]
       draw_text  "                  OTORGADO EL #{Auxiliar.fecha_formato(ticket.payment.credit.fecha_de_contrato)}",:size=>9, :at => [70,613+des]
       draw_text  "ATRASADO ACTUAL DE: #{Dinero.to_money at} ",:size=>9, :at => [70,600+des]
       
@@ -66,17 +68,18 @@ class ReciboPdf < Prawn::Document
       
       image "#{Rails.root}/app/assets/images/logo marjo.png", width: 155, height: 45, :at => [70,740+des]
       draw_text  "FOLIO:  2503616176B#{"0"*(5-ticket.id.to_s.length)+ticket.id.to_s}",:size=>9, :at => [420,720+des]
-      draw_text  "FECHA CARGO: #{ticket.payment.fecha_de_pago}" ,:size=>9, :at => [420,705+des]
-      draw_text  "SUCURSAL: #{ticket.payment.credit.padre.branch_office.nombre}",:size=>9, :at => [420,690+des]
-      draw_text  "AGENTE: #{ticket.payment.credit.padre.nombre_completo}",:size=>9, :at => [420,675+des]
+      draw_text  "FECHA : #{ticket.payment.fecha_de_pago}" ,:size=>9, :at => [420,705+des]
+      draw_text  "FUNDACION: #{ticket.payment.credit.padre.branch_office.nombre}",:size=>9, :at => [420,690+des]
+      draw_text  "PROMOTOR DE APOYO: #{ticket.payment.credit.padre.nombre_completo}",:size=>9, :at => [420,675+des]
   
       
-      draw_text  "COPIA 2 ",:size=>8, :at => [500,735+des]
+      draw_text  "ORIGINAL ",:size=>8, :at => [500,735+des]
       
       draw_text  "RECIBÍ DE : #{ticket.payment.credit.nombre_completo_deudor} ",:size=>9, :at => [70,665+des]
       draw_text  "LA CANTIDAD DE:  #{Dinero.to_money(ticket.cantidad)}",:size=>9, :at => [70,652+des]
       draw_text  "                     (#{(I18n.with_locale(:es) { (ticket.cantidad).to_i.to_words }).upcase } PESOS 00/100 M.N.)",:size=>9, :at => [70,639+des]
-      draw_text  "POR CONCEPTO DE: PAGO O ABONO AL CARGO #{ticket.payment.credit.product.payout.periocidad} #{ticket.payment.recibo} CORRESPONDIENTE AL CREDITO",:size=>9, :at => [70,626+des]
+      draw_text  "POR CONCEPTO DE: PAGO O ABONO AL CARGO #{ticket.payment.credit.product.payout.periocidad} #{ticket.payment.recibo} AL APOYO
+ECONÓMICO ",:size=>9, :at => [70,626+des]
       draw_text  "                  OTORGADO EL #{Auxiliar.fecha_formato(ticket.payment.credit.fecha_de_contrato)}",:size=>9, :at => [70,613+des]
       draw_text  "ATRASADO ACTUAL DE: #{Dinero.to_money at} ",:size=>9, :at => [70,600+des]
       
