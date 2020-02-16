2220e4534db81bde7ebb4437e4e9390c339eeaf9
adrianglezlop <entornoc9@gmail.com>
2020-02-16 16:25:05 -0700

    APOYO

--- app/pdf/corrida_pdf.rb
+++ app/pdf/corrida_pdf.rb
@@ -16,15 +16,14 @@ class CorridaPdf < Prawn::Document
    monto_a_pagar= @credit.monto_solicitud*(1+(@credit.taza_ord/100))
     image "#{Rails.root}/app/assets/images/logo marjo.png", width: 155, height: 45
     text "#{@credit.nombre_completo_deudor}"
-    table([["FECHA DE PRESTAMO", @credit.fecha_de_contrato.strftime("%d/%m/%Y")], 
-          ["MONTO PRESTAMO",Dinero.to_money(@credit.monto_solicitud)], 
+    table([["FECHA DEL APOYO ECONÓMICO", @credit.fecha_de_contrato.strftime("%d/%m/%Y")], 
+          ["MONTO DEL APOYO ECONÓMICO",Dinero.to_money(@credit.monto_solicitud)], 
           ["MONTO A PAGAR", Dinero.to_money(monto_a_pagar)], 
           ["PAGO FIJO", Dinero.to_money(monto_a_pagar/@credit.product.numero_de_pagos_a_realizar)], 
           #["TASA INTERES","#{'%.2f' % @credit.product.taza_de_interes_ordinaria}%"], 
-          ["TASA INTERES","#{'%.2f' % @credit.taza_ord}%"], 
+          ["BENEFICIO DE LA FUNDACION","#{'%.2f' % @credit.taza_ord}%"], 
           ["PLAZO "," #{@credit.product.numero_de_pagos_a_realizar} #{@credit.product.etiqueta_plural}"],  
-          ["PERIODICIDAD", @credit.product.payout.periocidad], 
-          ["CAT SIN IVA","#{'%.2f' % @credit.cat_sin_iva}%"]],:cell_style => { size: 8 })
+          ["PERIODICIDAD", @credit.product.payout.periocidad]]:cell_style => { size: 8 })
     move_down 20
     table(@arreglo,:cell_style => { size: 8 })
 
