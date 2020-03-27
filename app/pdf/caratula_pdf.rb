94cbb269543cded0c93d4ff4f04e83c80bddd48b
adrianglezlop <entornoc9@gmail.com>
2020-03-27 21:30:53 +0000

    cambios caratula

--- app/pdf/caratula_pdf.rb
+++ app/pdf/caratula_pdf.rb
@@ -8,7 +8,7 @@ class CaratulaPdf < Prawn::Document
   def text_content
       image open("#{Rails.root}/app/pdf/caratula_credito.jpg"), position: :center , :width=>560, :height=>790
       draw_text  "#{@credit.product.nombre_del_producto}",:size=>9, :at => [190,685]
-      draw_text  "#{@credit.product.loan.nombre}",:size=>9, :at => [190,666]
+      draw_text  "#{@credit.product.loan.nombre}",:size=>9, :at => [190,670]
       draw_text  "#{'%.2f' % @credit.taza_ord}%",:size=>9, :at => [200,610]
      draw_text  "#{'%.2f' % @credit.cat_sin_iva}%",:size=>9, :at => [55 ,619]
       draw_text  "#{Dinero.to_money(@credit.monto_solicitud)}",:size=>9, :at => [340,610]
@@ -19,8 +19,8 @@ class CaratulaPdf < Prawn::Document
       draw_text  "#{@credit.product.taza_de_interes_moratoria}",:size=>9, :at => [175,501]
       draw_text  "#{Conf.last.domicilio}",:size=>9, :at => [100,232]
       draw_text  "#{Conf.last.telefono}",:size=>9, :at => [100,215]
-      draw_text  "#{Conf.last.pagina}",:size=>9, :at => [140,203]
-      draw_text  "#{Conf.last.correo}",:size=>8, :at => [390,217]
+      draw_text  "#{Conf.last.pagina}",:size=>9, :at => [140,209]
+      draw_text  "#{Conf.last.correo}",:size=>8, :at => [390,209]
       
       #draw_text  "#{@credit.product.registro_del_contrado_de_adhesion}",:size=>9, :at => [240,123]
   end
