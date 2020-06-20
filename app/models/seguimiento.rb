54d38e9e27255847d0dd4398bffda09f9302b3d1
adrianglezlop <entornoc9@gmail.com>
2020-06-20 15:21:06 +0000

    cambio a seguimiento

--- app/models/seguimiento.rb
+++ app/models/seguimiento.rb
@@ -1,3 +1,7 @@
 class Seguimiento < ActiveRecord::Base
     belongs_to :payment
+    
+    def update(xcred,xcob, xfecha)
+        self.update(cobrado:monto).where("credit_id=xcred and fecha_corte=xfecha")
+    end
 end
