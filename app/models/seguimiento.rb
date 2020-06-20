b6e6b9b801f5c39e16c104102c6306cf492c935e
adrianglezlop <entornoc9@gmail.com>
2020-06-20 16:29:50 +0000

    cambio a seguimiento2

--- app/models/seguimiento.rb
+++ app/models/seguimiento.rb
@@ -3,10 +3,10 @@ class Seguimiento < ActiveRecord::Base
     
     def update(xcred,xcob, xfecha)
         #self.update(cobrado: 0).where("credit_id= ? and fecha_corte= ?",xcob,xcred, xfecha)
-        s=Seguimiento.where("credit_id= ? and fecha_corte= ?", xcred, xfecha).first
+        #s=Seguimiento.where("credit_id= ? and fecha_corte= ?", xcred, xfecha).first
        
-        s.cobrado == 1
-        s.save()
-        #return s.cobrado
+        #s.cobrado == 1
+        #s.save()
+
     end
 end
