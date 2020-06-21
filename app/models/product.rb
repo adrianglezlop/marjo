a3a627e688ae511eff98a880aee1d6f06e93bf7a
adrianglezlop <entornoc9@gmail.com>
2020-06-21 15:19:21 +0000

    cambios mas

--- app/models/product.rb
+++ app/models/product.rb
@@ -80,7 +80,7 @@ class Product < ActiveRecord::Base
      def modificar_seguimientos(fechainput)
           creditos = self.credits.where("status=1").order(:apellido_paterno)
           if Auxiliar.seguimiento_guardado_contador(creditos,fechainput) > 0
-               tuplas = Auxiliar.seguimiento_por_creditos(creditos,fechainput)
+               tuplas = Auxiliar.seguimiento_por_creditos_guardados(creditos,fechainput)
                tuplas.each do |t|
                     s=Seguimiento.where("credit_id= ? and fecha_corte= ?", t["credit_id"], fechainput).first
                     s.cobrado = t["cobrado"].to_f
